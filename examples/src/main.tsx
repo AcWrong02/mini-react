eb17cda1b6aae4a1163378173709a6962b5a36a2
AcWrong02 <3024800887@qq.com>
2026-01-23 22:03:17 +0800

    feat: 实现useMemo

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -1,70 +1,37 @@
-import { ReactDOM, useReducer, useState } from "../which-react";
+import { ReactDOM, useReducer, useState, useMemo } from "../which-react";
 import "./index.css";
 
 function FunctionComponent() {
-  const [count1, setCount1] = useReducer((x) => x + 1, 1);
-  const [count2, setCount2] = useState(1);
+  const [count1, setCount] = useReducer((x) => x + 1, 0);
+  const [count2, setCount2] = useState(0);
 
-  // const arr = count1 % 2 === 0 ? [0, 1, 2, 3, 4] : [0, 1, 2, 3];
-  // const arr = count1 % 2 === 0 ? [0, 1, 2, 3, 4] : [0, 1, 2, 4];
+  const expensive = useMemo(() => {
+    console.log("compute");
+    let sum = 0;
+    for (let i = 0; i < count1; i++) {
+      sum += i;
+    }
+    return sum;
+    //只有count变化，这里才重新执行
+  }, [count1]);
 
-  const arr = count1 % 2 === 0 ? [0, 1, 2, 3, 4] : [3, 2, 0, 4, 1];
+  // 昂贵运算
+  // const expensive = () => {
+  //   console.log("compute");
+  //   let sum = 0;
+  //   for (let i = 0; i < count1 * 100; i++) {
+  //     sum += i;
+  //   }
+  //   return sum;
+  //   //只有count变化，这里才重新执行
+  // };
 
-  // old 0, 1, 2, 4
-  // new 0, 1, 2, 3, 4
-  // 1个before 4
-
-  // old 3, 2, 0, 4, 1
-  // new 0, 1, 2, 3, 4
-  // 3个before null
-
-  const _cls = count1 % 2 === 0 ? "red green_bg" : "green red_bg";
-
-  // 0 删除
   return (
     <div className="border">
-      <h3 className={_cls}>函数组件</h3>
-      <button
-        onClick={() => {
-          setCount1();
-        }}
-      >
-        {count1}
-      </button>
-      <button
-        onClick={() => {
-          setCount2(count2 + 1);
-        }}
-      >
-        {count2}
-      </button>
-      <ul>
-        {arr.map((item) => (
-          <li key={"li" + item}>{item}</li>
-        ))}
-      </ul>
-
-      {count1 % 2 === 0 ? <h1>null</h1> : null}
-      {count1 % 2 === 0 ? <h1>undefined</h1> : undefined}
-      {count1 % 2 === 0 && <h1>boolean</h1>}
-
-      {/* {count1 % 2 === 0 ? (
-        <button
-          onClick={() => {
-            setCount1();
-          }}
-        >
-          {count1}
-        </button>
-      ) : (
-        <span
-          onClick={() => {
-            setCount1();
-          }}
-        >
-          react
-        </span>
-      )} */}
+      <h1>函数组件</h1>
+      <p>{expensive}</p>
+      <button onClick={() => setCount()}>{count1}</button>
+      <button onClick={() => setCount2(count2 + 1)}>{count2}</button>
     </div>
   );
 }
--- examples/which-react.ts
+++ examples/which-react.ts
@@ -1,2 +1,2 @@
-export { Component, useReducer, useState } from "../packages/react";
+export { Component, useReducer, useState, useMemo } from "../packages/react";
 export * as ReactDOM from "../packages/react-dom/client";
